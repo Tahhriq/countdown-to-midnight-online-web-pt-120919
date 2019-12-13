bf3a597f911ec4603241c14f6b72cd2204a2bc03
Tariq M Sulaiman <tariqsulaiman11@gmail.com>
2019-12-13 22:05:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,18 @@
 #write your code here
 
-def countdown
-  time = 10 
-  
-  while time > 0 
-    puts "#{time} SECOND(S)!"
-    time -= 1
-    
-    puts "HAPPY NEW YEAR!"
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
 
-sleep(num_secs)
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
